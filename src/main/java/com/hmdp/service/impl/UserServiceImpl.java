c35b357fcf2fe33a1027491350c439b875c53541
DHxingxing <3237779503@qq.com>
2025-02-11 19:21:44 +0800

    2025-02-11-19:21

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 /**
  * <p>
@@ -14,7 +22,47 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /*
+     * 实现发送短信验证码的功能
+     *   todo：接入第三方云平台
+     *  */
+
+
+    @Override
+    public Result senCode(String phone, HttpSession session) {
+//        校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute("code", code);
+        String code1 = (String) session.getAttribute("code");
+        System.out.println("生成的验证码是" + code1);
+        log.debug("发送成功，验证码: {}", code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+
+        // 1、校验手机号
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        // 2、校验验证码
+        if(session.getAttribute("code") == code){
+            return Result.fail("验证码格式错误");
+        }
+        // 3、不一致报错
+        // 4、一致根据手机号查询用户
+
+
+        return null;
+    }
 }

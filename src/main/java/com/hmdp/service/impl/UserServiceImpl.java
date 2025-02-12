18f6154be71fa9e9222f98d4fa29d5cc175e493c
DHxingxing <3237779503@qq.com>
2025-02-12 10:39:04 +0800

    2025-02-12-10:39

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -14,6 +14,9 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpSession;
 import java.util.Random;
 
+import static com.hmdp.utils.SystemConstants.CACHE_CODE;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
 /**
  * <p>
  * 服务实现类
@@ -56,13 +59,32 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("手机号格式错误");
         }
         // 2、校验验证码
-        if(session.getAttribute("code") == code){
-            return Result.fail("验证码格式错误");
-        }
+        String code = loginForm.getCode();
+        CACHE_CODE = (String) session.getAttribute("code");
         // 3、不一致报错
-        // 4、一致根据手机号查询用户
 
+        if(CACHE_CODE == null){
+            return Result.fail("验证码已经过期");
+        }
+        if(!CACHE_CODE.equals(code)){
+            return Result.fail("验证码错误");
+        }
+        // 4、一致根据手机号查询用户 select * from tb_user where phone = ?
+        User user = query().eq("phone", phone).one();
+        if(user == null){
+            // 不存在则 创建用户
+            user = createUserWithPhone(phone);
+        }
+        session.setAttribute("user",user);
+        return Result.ok();
+    }
 
-        return null;
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(6));
+        save(user); // 并没有保存到数据库中？ 为什么？
+        return user;
+
     }
 }
